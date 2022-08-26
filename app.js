5243b9e6063ebd0c5c7ebb1a017f222f78696e8f
Andesaputra <andesaputra.developer@gmail.com>
2022-08-26 16:36:52 +0700

    fourth commit

---
+++ app.js
@@ -0,0 +1,41 @@
+var trigger = "backnav";
+var scrollTrigger = 60;
+
+window.onscroll = function() {
+  if (window.scrollY >= scrollTrigger || window.pageYOffset >= scrollTrigger) {
+    document.getElementsByTagName("nav")[0].classList.add(trigger);
+  } else {
+    document.getElementsByTagName("nav")[0].classList.remove(trigger);
+  }
+};
+
+// Carousel BestSeller
+
+const gap = 30;
+
+const carousel = document.getElementById("carousel"),
+  content = document.getElementById("content"),
+  next = document.getElementById("nav-next"),
+  prev = document.getElementById("nav-prev");
+
+next.addEventListener("click", e => {
+  carousel.scrollBy(width + gap, 0);
+  if (carousel.scrollWidth !== 0) {
+    prev.style.display = "flex";
+  }
+  if (content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+prev.addEventListener("click", e => {
+  carousel.scrollBy(-(width + gap), 0);
+  if (carousel.scrollLeft - width - gap <= 0) {
+    prev.style.display = "flex";
+  }
+  if (!content.scrollWidth - width - gap <= carousel.scrollLeft + width) {
+    next.style.display = "flex";
+  }
+});
+
+let width = carousel.offsetWidth;
+window.addEventListener("resize", e => (width = carousel.offsetWidth));
